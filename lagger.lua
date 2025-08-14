fa9f15fa5c96c5495e60323867d80757ccece2c3
xtvoo <haydencalvert2020@gmail.com>
2025-08-14 13:35:55 +0100

    Update lagger.lua

--- lagger.lua
+++ lagger.lua
@@ -2,7 +2,7 @@ local players = game:GetService("Players")
 local chat = game:GetService("TextChatService")
 local runservice = game:GetService("RunService")
 local player = players.LocalPlayer
-local owner_name = "Haremelito"
+local owner_name = "CaseohIsA_StandUser"
 local local_player = player
 
 if player.Name == owner_name then return end
@@ -779,3 +779,4 @@ chat.MessageReceived:Connect(function(msg)
         end
     end
 end)
+
